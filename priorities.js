c3f2724b08e624d7940d728a894be3c488f3b9cb
Jonathan Arnett <jonarnett90@gmail.com>
2015-05-08 01:05:57 -0400

    Implemented a sort of sorting

--- priorities.js
+++ priorities.js
@@ -1,16 +1,24 @@
-Tasks = new Mongo.Collection("tasks");
+const QUANTA        = 3600000;   // 1 hour
+const TIME_PER_DAY  = 28800000;  // 8 hours
+const MS_PER_DAY    = 86400000;  // 24 hours
+
+var error_message = '';
 
 if (Meteor.isClient) {
     Template.body.helpers({
         top_task: function() {
-            Meteor.call('getSortedTasks').first;
+            return Tasks.findOne({checked: false}, {sort: {start_date: 1, priority: 1}})
         },
         tasks: function() {
-            return Tasks.find({checked: false}, {skip: 1});
+            return Tasks.find({checked: false}, {sort: {start_date: 1, priority: 1}, skip: 1});
         },
         finished_tasks: function() {
             return Tasks.find({checked: true}, {limit: 5});
-        }
+        },
+        sorted_tasks: function() {
+            return Tasks.find({checked: false})
+        },
+        error_message: function() { return error_message; }
     });
 
 
@@ -18,18 +26,35 @@ if (Meteor.isClient) {
         'submit .new-task-form': function(event) {
             event.preventDefault();
 
-            Tasks.insert({
+            // Calculate ms to completion
+            var ms_to_completion = parseInt(event.target.ttc.value) * 3600000;
+
+            // Calculate days needed, multiply by ms per day, and subtract from due date
+            var due_date = new Date(event.target.due_date.value);
+            var start_date = new Date(due_date - (ms_to_completion / TIME_PER_DAY) * MS_PER_DAY);
+
+            var id = Tasks.insert({
                 text: event.target.text.value,
-                priority: event.target.priority.value,
-                due_date: new Date(event.target.due_date.value),
+                priority: parseInt(event.target.priority.value),
+                due_date: due_date,
+                time_to_completion: ms_to_completion,
+                start_date: start_date,
                 created_at: new Date(),
                 checked: false
+            }, function(err, result) {
+                if (err) {
+                    error_message = err;
+                    console.log(err);
+                }
             });
+
+            console.log(id);
 
             // Clear the inputs
             event.target.text.value = "";
             event.target.priority.value = "";
             event.target.due_date.value = "";
+            event.target.ttc.value = "";
         }
     });
 
@@ -44,26 +69,4 @@ if (Meteor.isServer) {
     Meteor.startup(function () {
         // code to run on server at startup
     });
-
-    Meteor.methods({
-        getSortedTasks: function() {
-            tasks = Tasks.find({});
-
-            return tasks.sort(function(a, b) {
-                var pri_weight = 0.4;
-                var date_weight = 0.6;
-
-                function days_until(date) {
-                    return (new Date() - date) / (1000 * 60 * 60 * 24);
-                }
-
-                var a_weight = pri_weight * a.priority +
-                    date_weight * (days_until(a.due_date));
-                var b_weight = pri_weight * b.priority +
-                    date_weight * (days_until(b.due_date));
-
-                return a_weight - b_weight;
-            });
-        }
-    })
 }
